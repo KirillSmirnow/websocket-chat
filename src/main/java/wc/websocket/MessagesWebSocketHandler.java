bc00918a95199b7f618389fbfe6fe4c9bf0c545c
Kirill Smirnov <smirnowku@gmail.com>
2022-11-29 23:42:25 +0300

    WebSocket authentication: incoming message

--- src/main/java/wc/websocket/MessagesWebSocketHandler.java
+++ src/main/java/wc/websocket/MessagesWebSocketHandler.java
@@ -7,12 +7,14 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
-import wc.security.UserAuthentication;
+import wc.model.Session;
+import wc.repository.SessionRepository;
 import wc.service.message.MessageView;
 import wc.service.message.NewMessageNotifier;
 
 import java.util.Map;
 import java.util.Set;
+import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Component
@@ -21,14 +23,14 @@ public class MessagesWebSocketHandler extends TextWebSocketHandler implements Ne
 
     private final Map<Long, Set<WebSocketSession>> sessionsByUserId = new ConcurrentHashMap<>();
 
+    private final SessionRepository sessionRepository;
     private final ObjectMapper objectMapper;
 
     @Override
-    public void afterConnectionEstablished(WebSocketSession session) {
-        if (session.getPrincipal() instanceof UserAuthentication userAuthentication) {
-            var userId = userAuthentication.getUserId();
-            sessionsByUserId.computeIfAbsent(userId, $ -> ConcurrentHashMap.newKeySet()).add(session);
-        }
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) {
+        var token = UUID.fromString(message.getPayload());
+        var user = sessionRepository.findById(token).map(Session::getUser).orElseThrow();
+        sessionsByUserId.computeIfAbsent(user.getId(), $ -> ConcurrentHashMap.newKeySet()).add(session);
     }
 
     @Override
